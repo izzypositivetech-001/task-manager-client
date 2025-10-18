82f98b76961401ad88168cf2946e76b32bfb02b9
izzypositivetech-001 <izzypositivetech@gmail.com>
2025-10-18 15:23:08 +0100

    modal/selectuser

--- src/components/inputs/SelectUsers.jsx
+++ src/components/inputs/SelectUsers.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import axiosInstance from '../../utils/axiosInstance';
 import { API_PATHS } from '../../utils/apiPaths';
+import { LuUsers } from "react-icons/lu";
+import Modal from '../Modal';
 
 const SelectUsers = ({selectedUsers, setSelectedUsers}) => {
     const [allUsers, setAllUsers] = useState([]);
-    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(true);
     const [tempSelectedUsers, setTempSelectedUsers] = useState([]);
 
     const getAllUsers = async () => {
@@ -36,6 +38,10 @@ const SelectUsers = ({selectedUsers, setSelectedUsers}) => {
     .map((user) => user.profileImageUrl);
 
     useEffect(() => {
+        getAllUsers();
+    }, []);
+
+    useEffect(() => {
         if (selectedUsers.length === 0) {
             setTempSelectedUsers([]);
         }
@@ -43,8 +49,58 @@ const SelectUsers = ({selectedUsers, setSelectedUsers}) => {
         return () => {};
     }, [selectedUsers])
   return (
-    <div>
-      select user
+    <div className='space-y-4 mt-2'>
+      {selectedUserAvatars.length === 0 && (
+        <button className='card-btn' onClick={() => setIsModalOpen(true)}>
+            <LuUsers className="text-sm" /> Add Member
+        </button>
+      )}
+
+      <Modal 
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        title="Select Users"
+      >
+        <div className='space-y-4 h-[60vh] overflow-y-auto'>
+            {allUsers.map((user) => (
+                <div 
+                className='flex items-center gap-4 p-3 border-b border-gray-200 '
+                key={user.id}
+                >
+                    <img 
+                    src={user.profileImageUrl} 
+                    alt={user.name}
+                    className='w-10 h-10 rounded-full'
+                     />
+
+                     <div className='flex-1'>
+                        <p className='font-medium text-gray-800 dark:text-white'>
+                            {user.name}
+                        </p>
+                        <p className='text-[13px] text-gray-500'>
+                            {user.email}
+                        </p>
+                     </div>  
+
+                     <input 
+                        type='checkbox'
+                        checked={tempSelectedUsers.includes(user._id)}
+                        onChange={() => toggleUserSelection(user._id)}
+                        className='w-4 h-4 text-primary bg-gray-100 border-gray-300 rounded-sm outline-none'
+                     />
+                </div>
+            ))}
+        </div>
+
+        <div className='flex justify-end gap-4 pt-4'>
+            <button className='card-btn' onClick={() => setIsModalOpen(false)}>
+                CANCEL
+            </button>
+            <button className='card-btn-fill' onClick={handleAssign}>
+                DONE
+            </button>
+        </div>
+      </Modal>
     </div>
   )
 }
