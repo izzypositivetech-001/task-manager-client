ad84be5630fe1b2edcfe74f623fedc2fb3e7fa30
izzypositivetech-001 <izzypositivetech@gmail.com>
2025-10-26 21:53:50 +0100

    Input corrected

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from "react";
 import AuthLayout from "../../components/layouts/AuthLayout";
-import Input from "../../components/inputs/input";
+import Input from "../../components/inputs/Input";
 import { Link, useNavigate } from "react-router-dom";
 import { validateEmail } from "../../utils/helper";
 import { UserContext } from "../../context/userContext";
