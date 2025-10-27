b20b98731d0da5d1f5d80c82ba09941dd123c213
izzypositivetech-001 <izzypositivetech@gmail.com>
2025-10-27 09:37:03 +0100

    modified-utils

--- src/utils/apiPaths.js
+++ src/utils/apiPaths.js
@@ -1,42 +1,43 @@
-export const BASE_URL = import.meta.env.VITE_API_BASE_URL ||   "http://localhost:5000"
+export const BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
 
 // utils/apiPaths.js
 export const API_PATHS = {
-    AUTH: {
-        REGISTER: "/api/auth/register", //register a new user (Admin or Member)
-        LOGIN: "/api/auth/login", //Autheticate user & return JWT token
+  AUTH: {
+    REGISTER: "/api/auth/register", //register a new user (Admin or Member)
+    LOGIN: "/api/auth/login", //Autheticate user & return JWT token
     REFRESH: "/api/auth/refresh",
     LOGOUT: "/api/auth/logout",
-        GET_PROFILE: "/api/auth/profile", //Get logged-in user details
-    },
+    GET_PROFILE: "/api/auth/profile", //Get logged-in user details
+  },
 
-    USERS: {
-        GET_ALL_USERS: "/api/users", //Get all users (Admin only)
-        GET_USER_BY_ID: (userId) => `/api/users/${userId}`, //Get user by ID
-        CREATE_USER: "/api/users", //Create a new user (Admin only)
-        UPDATE_USER: (userId) => `/api/users/${userId}`, //Update user details
-        DELETE_USER: (userId) => `/api/users/${userId}` //Delete a user
-    },
+  USERS: {
+    GET_ALL_USERS: "/api/users", //Get all users (Admin only)
+    GET_USER_BY_ID: (userId) => `/api/users/${userId}`, //Get user by ID
+    CREATE_USER: "/api/users", //Create a new user (Admin only)
+    UPDATE_USER: (userId) => `/api/users/${userId}`, //Update user details
+    DELETE_USER: (userId) => `/api/users/${userId}`, //Delete a user
+  },
 
-    TASKS: {
-        GET_DASHBOARD_DATA:  "/api/tasks/dashboard-data", //Get Dashboard Data
-        GET_USER_DASHBOARD_DATA: "/api/tasks/user-dashboard-data", //Get User Dashboard data
-        GET_ALL_TASKS: "/api/tasks", //Get all tasks (Admin: all, User: only assigned)
-        GET_ALL_TASK_BY_ID: (taskId) => `/api/tasks/${taskId}` ,// Get task by Id
-        CREATE_TASK: "/api/tasks", //Create a new task (Admin only)
-        UPDATE_TASK: (taskId) => `/api/tasks/${taskId}`, // Update task details
-        DELETE_TASK: (taskId) => `/api/tasks/${taskId}`, //Delete a task (Admin only)
+  TASKS: {
+    GET_DASHBOARD_DATA: "/api/tasks/dashboard-data", //Get Dashboard Data
+    GET_USER_DASHBOARD_DATA: "/api/tasks/user-dashboard-data", //Get User Dashboard data
+    GET_ALL_TASKS: "/api/tasks", //Get all tasks (Admin: all, User: only assigned)
+    GET_ALL_TASK_BY_ID: (taskId) => `/api/tasks/${taskId}`, // Get task by Id
+    CREATE_TASK: "/api/tasks", //Create a new task (Admin only)
+    UPDATE_TASK: (taskId) => `/api/tasks/${taskId}`, // Update task details
+    DELETE_TASK: (taskId) => `/api/tasks/${taskId}`, //Delete a task (Admin only)
 
-        UPDATE_TASK_STATUS: (taskId) => `/api/tasks/${taskId}/status`, // Update task
-        UPDATE_TODO_CHECKLIST: (taskId) => `/api/tasks/${taskId}/todo` // Update todo checklist
-    },
+    UPDATE_TASK_STATUS: (taskId) => `/api/tasks/${taskId}/status`, // Update task
+    UPDATE_TODO_CHECKLIST: (taskId) => `/api/tasks/${taskId}/todo`, // Update todo checklist
+  },
 
-    REPORTS: {
-        EXPORT_TASKS: "/api/reports/export/tasks", //Download all tasks as an exel/pdf report
-        EXPORT_USERS: "/api/reports/export/users", //Download user-task report
-    },
+  REPORTS: {
+    EXPORT_TASKS: "/api/reports/export/tasks", //Download all tasks as an exel/pdf report
+    EXPORT_USERS: "/api/reports/export/users", //Download user-task report
+  },
 
-    IMAGE: {
-        UPLOAD_IMAGE: "/api/auth/upload-image",
-    },
-}
+  IMAGE: {
+    UPLOAD_IMAGE: "/api/auth/upload-image",
+  },
+};
